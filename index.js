7e98ad2bd9a863cc84ba16e46010d56e43a46124
J <jelanic12@nycstudents.net>
2024-01-04 10:43:21 -0500

    g

--- index.js
+++ index.js
@@ -3,3 +3,22 @@ const app = express();
 
 app.listen(6789, () => console.log("Server is running"));
 app.use(express.static('public'));
+app.use(express.json({ limit: "1mb" }));
+
+
+app.post('/api', (req, res) => {
+    console.log("I got a request");
+    console.log(req.body);
+
+    res.json({
+        status: 'it works',
+        latitude: req.body.lat,
+        longitude: req.body.lon
+    });
+  
+})
+
+app.get("/d", (req, res) => {
+    console.log("d")
+})
+  
