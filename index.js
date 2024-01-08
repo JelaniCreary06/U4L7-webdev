de1c977b26f8eb09e6923ce554ea5c8f84cc8872
JelaniSchoolAccount <antwinecreary@gmail.com>
2024-01-07 22:12:19 -0500

    blerp

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
 app.listen(6789, () => console.log("Server is running"));
 app.use(express.static('public'));
 app.use(express.json({ limit: "1mb" }));
-
 
 app.post('/api', (req, res) => {
     console.log("I got a request");
@@ -16,6 +16,9 @@ app.post('/api', (req, res) => {
         longitude: req.body.lon
     });
   
+    const text = fs.readFileSync('public/locations.txt', 'utf-8')
+
+    fs.writeFileSync('public/locations.txt', `${text}${req.body.lat}!${req.body.lon},`)
 })
 
 app.get("/d", (req, res) => {
